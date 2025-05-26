3fb91f8ba8c7e78ddd036ff7ba38c71fd6ec2669
kamalkumar <kamalelango15@gmail.com>
2025-05-26 23:14:48 +0530

    added new project

---
+++ MauiPhoneContactLibrary/Helper/ContactGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MauiPhoneContactLibrary.Helper
+{
+    public class ContactGroup:List<ContactItem>
+    {
+        public string Title { get; set; }
+        public string ShortTitle { get; set; }
+        public ContactGroup(string title, string shortTitle)
+        {
+            Title = title;
+            ShortTitle = shortTitle;
+        }
+    }
+}
---
+++ MauiPhoneContactLibrary/Interface/IContact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MauiPhoneContactLibrary
+{
+    public interface IContact
+    {
+        Dictionary<string, object> GetAllContact();
+        void CheckPermission();
+        event EventHandler CustomPermissionStatus;
+    }
+}
