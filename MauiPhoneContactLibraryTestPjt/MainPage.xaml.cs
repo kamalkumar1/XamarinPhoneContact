9dc8a84a8d94281cb1beb7b9a76ab22693c0ff6d
kamalkumar <kamalelango15@gmail.com>
2025-05-26 23:45:35 +0530

    added new project

---
+++ MauiPhoneContactLibraryTestPjt/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+
+using MauiPhoneContactLibrary;
+using MauiPhoneContactLibrary.Helper;
+
+namespace MauiPhoneContactLibraryTestPjt
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                kkContactControl.EnableMultiSelectionTickMark = true;
+                kkContactControl.CloseButtonImageName = "deletebutton.png";
+                IContact contact = new ContactList(); // Assuming Contact implements IContact
+                MobileContact mobile = new MobileContact(contact);
+                mobile.getSelectedContact += Mobile_GetSelectedContactItem;
+                await Navigation.PushModalAsync(mobile);
+
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+
+            }
+        }
+    }
+
+}
---
+++ MauiPhoneContactLibraryTestPjt/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiPhoneContactLibraryTestPjt
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
